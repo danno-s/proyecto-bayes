7e7e1b8802bc7a00dec42a935546ebb2a26f7caa
Pablo Guerrero <pguerrer@ing.uchile.cl>
2015-10-27 23:06:41 -0300

    versión que guarda las URLs (repetidas)

--- src/dataParsing/ExtractURLs.py
+++ src/dataParsing/ExtractURLs.py
@@ -4,20 +4,39 @@ import json
 import mysql.connector
 
 with open('connections.json', 'r') as f:
-    connectionsJSON = f.read()
-    
+	connectionsJSON = f.read()
+	
 connections = json.loads(connectionsJSON)
 
-conn = connections[0]
-print(conn)
-sql='select * from pageview'
-cnx = mysql.connector.connect(user=conn['user'], password=conn['passwd'], host=conn['host'],database=conn['db'])
+connRead = connections[0]
+connWrite = connections[1]
+
+#print(connRead)
+#print(connWrite)
+
+sqlRead = 'select * from pageview'
+cnx = mysql.connector.connect(user=connRead['user'], password=connRead['passwd'], host=connRead['host'],database=connRead['db'])
 cursor = cnx.cursor()
 
-cursor.execute(sql)
+cursor.execute(sqlRead)
 rows = cursor.fetchall()
 
+L=[]
+
 for row in rows:
-    print(row[4])
-        
+	L.extend(row[4].split(" "))
+
+cnx.close()
+
+sqlWrite = "INSERT INTO urls (url) VALUES ("
+
+cnx = mysql.connector.connect(user=connWrite['user'], password=connWrite['passwd'], host=connWrite['host'],database=connWrite['db'])
+cursor = cnx.cursor()
+
+for url in L:
+	sql = sqlWrite + '"' + url + '"'
+	print(sql)
+	cursor.execute(sqlWrite + '"' + url + '");')
+
+cnx.commit()
 cnx.close()

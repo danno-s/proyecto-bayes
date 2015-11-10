4d90861d50544ab48ff9a72be0f7ebefa4b84372
Constanza <coniis@hotmail.com>
2015-11-10 19:20:03 -0300

    Añade el script

---
+++ src/featureExtraction/extractUserClusteringFeatures.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import json
+import mysql.connector
+
+
+with open('connections.json', 'r') as f:
+	connectionsJSON = f.read()
+
+connections = json.loads(connectionsJSON)
+
+connGC = connections[0]
+connPD = connections[1]
+
+
+sqlRead = 'select url, variables from pageview'
+cnx = mysql.connector.connect(user=connGC['user'], password=connGC['passwd'], host=connGC['host'],database=connGC['db'])
+cursor = cnx.cursor()
+
+cursor.execute(sqlRead)
+usersUrls = cursor.fetchall()
+
+cnx.close()
+
+sqlRead = 'select url from urls'
+cnx = mysql.connector.connect(user=connPD['user'], password=connPD['passwd'], host=connPD['host'],database=connPD['db'])
+cursor = cnx.cursor()
+
+cursor.execute(sqlRead)
+urls = cursor.fetchall()
+
+urls = [str(item[0]) for item in urls]
+
+D = dict()
+
+for row in usersUrls:
+    l = row[1].split(";")
+    id = int(l[3][l[3].rfind(":")+2:-1])
+    l = row[0].split(" ")
+    if id in D:
+        v = D[id]
+    else:
+        v = [0]*len(urls)
+    for i in range(len(urls)):
+        if urls[i] in l:
+            v[i] = 1
+    D[id] = v
+
+for key in D.keys():
+    D[key] = str(tuple(D[key]))
+
+print(D)
+
+sqlWrite = ("INSERT INTO userclusteringfeatures (id_usuario,userFeatureVector) VALUES (%s, %s)")
+
+for item in D.items():
+	cursor.execute(sqlWrite,item)
+
+cnx.commit()
+cnx.close()

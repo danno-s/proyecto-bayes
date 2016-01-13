a589e1da620faa2591f6b41317dabef61ba57084
cescobarf <coniis@hotmail.com>
2016-01-13 11:23:27 -0300

    extractUserClusteringFeatures arreglado, compara usando llaves hash, y genera los vectores correctamente

--- src/featureExtraction/extractUserClusteringFeatures.py
+++ src/featureExtraction/extractUserClusteringFeatures.py
@@ -35,27 +35,30 @@ cursor = cnx.cursor()
 cursor.execute(sqlRead)
 urls = cursor.fetchall()
 
-urlHash = [item[0] for item in urls]
+urls = [item[0] for item in urls]
 
 D = dict()
 
 for row in usersUrls:
     l = loads(bytes(row[1], 'UTF-8'))
-    id = int(l[b'id_usuario'].decode("utf-8"))
-    l = hash(row[0])
-    if id in D:
-        v = D[id]
-    else:
-        v = [0]*len(urls)
-    for i in range(len(urls)):
-        if urls[i] in l:
-            v[i] = 1
-    D[id] = v
+    try:
+        id = int(l[b'id_usuario'].decode("utf-8"))
+        l = hash(json.dumps(json.loads(row[0])))
+        if id in D:
+            v = D[id]
+        else:
+            v = [0]*len(urls)
+        for i in range(len(urls)):
+            if urls[i] == l: #PORQUE NO SON IGUALES!!!!!!!!!!!!!!!
+                v[i] = 1
+        D[id] = v
+    except TypeError:
+        print("Texto no corresponde a datos de usuario, variable leida = "+str(l))
 
 for key in D.keys():
     D[key] = str(tuple(D[key]))
 
-print(D)
+cursor.execute("TRUNCATE userclusteringfeatures")
 
 sqlWrite = ("INSERT INTO userclusteringfeatures (id_usuario,userFeatureVector) VALUES (%s, %s)")
 

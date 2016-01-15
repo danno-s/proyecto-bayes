dab15921c741eb8212137a4580fe14e9a4a9db44
sebastian <sebago.nc@gmail.com>
2016-01-15 17:57:59 -0300

    creado package clustering para los script que realizan clustering.

--- src/utils/loadConfig.py
+++ src/utils/loadConfig.py
@@ -1,11 +1,12 @@
 import json
 import os
 
-with open(os.path.dirname(os.path.dirname(__file__)) + '/connections.json', 'r') as f:
+with open(os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + '/config.json', 'r') as f:
     configurationJSON = f.read()
 
+parameters = json.loads(configurationJSON)
+print(parameters)
 
 class Config:
-    parameters = json.loads(configurationJSON)
     def __init__(self):
         pass

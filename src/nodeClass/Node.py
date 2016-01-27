2fe226d01bc0768022d444550e42223d1219128b
cescobarf <coniis@hotmail.com>
2016-01-27 15:44:49 -0300

    Clase Node arreglada

--- src/nodeClass/Node.py
+++ src/nodeClass/Node.py
@@ -9,30 +9,31 @@ from .MicroNode import MicroNode
 
 class Node(object):
 
-    def __init__(self, profile, url, micro=()):
-        self.profile = profile
-        self.url = url
-        self.microNode = MicroNode(micro)
-        self.next = []
+    def __init__(self, str):
+        self.id = str[0][0]
+        self.profile = str[0][4]
+        self.id_url = str[0][3]
+        self.microNode = str[0][5]
+        self.id_user = str[0][1]
+        self.clickdate = str[0][2]
+        self.next = None
 
-    def defMicroNode(self, state):
-        self.microState = state
+    def defMicroNode(self, id):
+        self.microNode = id
 
     def addNext(self, node):
-        self.next.append(node)
+        self.next = node
 
     def belongs(self, node):
-        return self.profile == node.profile and self.url == node.url
+        return self.profile == node.profile and self.id_url == node.id_url
 
-    def equals(self, node):
-        return self.equal(node) and self.microNode.equal(node.microState)
+    def equal(self, node):
+        return self.belongs(node) and self.microNode.equal(node.microNode)
 
     def toJson(self):
-        string = "{ id : " + random.randint() + "," #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        string += "profile : " + str(self.profile) + ","
-        string += "url : " + self.url + ","
-        string += "MicroNode : " + json.dumps(self.microNode.toJson()) + "}"
-        return json.loads(string)
+        Dict = dict(id_node=self.id, id_user=self.id_user, profile=self.profile, id_url=self.id_url,
+                    clickdate=self.clickdate,microNode=self.microNode)
+        return json.dumps(Dict)
 
     def __str__(self):
-        return str(self.profile) + self.url + str(self.microNode)
+        return str(self.profile) + self.id_url + str(self.microNode)

0b42e4fc7790eac000d5b5bd515ed9ead3ae7ad7
sebastian <sebago.nc@gmail.com>
2016-01-27 18:41:29 -0300

    cambiado constructor y funcion __str__

--- src/nodeClass/Node.py
+++ src/nodeClass/Node.py
@@ -9,13 +9,24 @@ from .MicroNode import MicroNode
 
 class Node(object):
 
-    def __init__(self, str):
-        self.id = str[0][0]
-        self.profile = str[0][4]
-        self.id_url = str[0][3]
-        self.microNode = str[0][5]
-        self.id_user = str[0][1]
-        self.clickdate = str[0][2]
+    def __init__(self, str=None,id=None, id_user=None,profile=None,id_url=None,microNode= None):
+        if str:
+            self.id = str[0][0]
+            self.id_user = str[0][1]
+            self.clickdate = str[0][2]
+            self.id_url = str[0][3]
+            self.profile = str[0][4]
+            self.microNode = str[0][5]
+        if id:
+            self.id = id
+        if id_user:
+            self.id_user = id_user
+        if id_url:
+            self.id_url = id_url
+        if profile:
+            self.profile = profile
+        if microNode:
+            self.microNode = microNode
         self.next = None
 
     def defMicroNode(self, id):
@@ -36,4 +47,4 @@ class Node(object):
         return json.dumps(Dict)
 
     def __str__(self):
-        return str(self.profile) + self.id_url + str(self.microNode)
+        return str((self.profile, self.id_url,self.microNode))

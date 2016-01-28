f594ef346bec0293f0d4c4370a4b409aa088c3cd
sebastian <sebago.nc@gmail.com>
2016-01-28 15:03:41 -0300

    Refactoring de sessionParser para que envíe nodos de cada usuario a los sessionizadores

--- src/sessionParser/SessionParser.py
+++ src/sessionParser/SessionParser.py
@@ -1,14 +1,15 @@
 from src.sessionParser.sessionizers.Sessionizer import Sessionizer
-from src.utils.sqlUtils import sqlWrapper
 from src.nodeClass.Node import Node
+from src.utils.dataParsingUtils import *
 
 class SessionParser:
 
     def __init__(self,s):
         assert isinstance(s,Sessionizer)
-        self.nodes = list()
-        self.sessionizer = s
+        self.nodesD = dict()
         self.__loadNodes()
+        self.sessionizer = s
+
         self.sessions = list()
 
     def parseSessions(self):
@@ -18,10 +19,23 @@ class SessionParser:
         for s in self.sessions:
             print(s)
     def __loadNodes(self):
+        # Obtener todos los usuarios.
+        userL = getAllUserIDs()
+        assert len(userL)>0
+
+        # Extraer datos de nodos para cada usuario
+        userSessionsD = dict()
         sqlCD = sqlWrapper('CD')
-        rows = sqlCD.read("SELECT clickDate,user_id,urls_id,profile,micro_id from nodes")
-        for row in rows:
-            self.nodes.append((row[0],int(row[1]),row[2],row[3],row[4]))  # (clickDate, user_id, urls_id, profile, micro_id)
+
+        for user_id in userL:
+            #rows= sqlCD.read("SELECT clickDate,user_id,urls_id,profile,micro_id from nodes WHERE user_id="+str(user_id))
+            rows= sqlCD.read("SELECT * from nodes WHERE user_id="+str(user_id))
+            user_steps = [Node(str=row) for row in rows]                            #(row[0],int(row[1]),row[2],row[3],row[4]) for row in rows] # (clickDate, user_id, urls_id, profile, micro_id)
+            if len(user_steps)>0:
+                self.nodesD[user_id] = user_steps
+
+        #for k,v in self.nodesD.items():
+        #    print(str(k)+": "+' '.join([str(x) for x in v]))
 
 
 if __name__ == "__main__":
@@ -29,7 +43,7 @@ if __name__ == "__main__":
     f = SessionParser(FilteredSessionizer())
     f.parseSessions()
     f.printSessions()
-    print('\n')
+ #   print('\n')
 #    from src.sessionParser.sessionizers.CompleteSessionizer import CompleteSessionizer
 #    a = SessionParser(CompleteSessionizer())
 #    a.parseSessions()
--- src/sessionParser/sessionizers/FilteredSessionizer.py
+++ src/sessionParser/sessionizers/FilteredSessionizer.py
@@ -5,6 +5,7 @@ from src.utils.dataParsingUtils import *
 from src.nodeClass.Node import Node
 from src.sessionClass.Session import Session
 
+
 class FilteredSessionizer(Sessionizer):
     def __init__(self):
         Sessionizer.__init__(self)
@@ -16,61 +17,52 @@ class FilteredSessionizer(Sessionizer):
         return [(x[0],x[2],x[3],x[4]) for x in nodes if x[1] == user_id]
                                                                 # (clickDate, urls_id, profile, micro_id)
 
-    def extractFilteredSessionsOf(self,user_id,nodes):
+    def extractFilteredSessionsOf(self,nodesL):
         sessions = list()
-        userSessions= self.getNodesOf(user_id,nodes)
-        if len(userSessions) == 0: return sessions
-        profile = userSessions[0][2]
-        prevStep = userSessions[0]
-        initTime = prevStep[0] #tiempo del primer dato.
-        macro_id = prevStep[1]
-        micro_id = prevStep[3]
+        if len(nodesL) == 0: return sessions
+        profile = nodesL[0].profile
+        firstNode = nodesL[0]
 
-        sessionData = list()    # datos de sesión actual.
+        prevStep = firstNode    # datos de sesión actual.
+        initTime = prevStep.clickdate #tiempo del primer dato.
+        macro_id = prevStep.urls_id
+        micro_id = prevStep.microNode
 
-        for i, step in enumerate(userSessions[1:]):
-            macro_id = step[1]
-            micro_id = step[3]
-            if step[0] - prevStep[0] <= self.tlimit:   # condición para mantenerse en sesión actual
+        for i, step in enumerate(nodesL[1:]):
+            macro_id = step.urls_id
+            micro_id = step.microNode
+            if step.clickdate - prevStep.clickdate <= self.tlimit:   # condición para mantenerse en sesión actual
                 if self.filter.accepts(prevStep,step):
-                    sessionData.append([prevStep[1],prevStep[3]])                 # Agregar datos a sesión actual
+                    prevStep.addNext(step)                 # Agregar datos a sesión actual
             else:
-                endTime = prevStep[0]
-                sessionData.append([prevStep[1],prevStep[3]])
-                sessions.append(self.__toSession(profile,sessionData,initTime,endTime))  # guardar sesión actual del usuario
-
-                sessionData.clear()
-                sessionData.append([step[1],step[3]])     # inicializar nueva sesión
-                initTime = step[0]
+                endTime = prevStep.clickdate
+                prevStep.addNext(step)
+                sessions.append(self.__toSession(profile,firstNode,initTime,endTime))  # guardar sesión actual del usuario
+                firstNode = step     # inicializar nueva sesión
+                initTime = step.clickdate
 
             prevStep = step # actualizar step previo.
         else:
             sessionData.append([macro_id,micro_id])     # inicializar nueva sesión
-            endTime = prevStep[0]
-            sessions.append(self.__toSession(profile,sessionData,initTime,endTime))   # guardar última sesión del usuario.
+            endTime = prevStep.clickdate
+            sessions.append(self.__toSession(profile, sessionData, initTime, endTime))   # guardar última sesión del usuario.
 
         return sessions
 
     def sessionize(self,sParser):
-        nodes = sParser.nodes # (clickDate, user_id, urls_id, profile,micro_id)
         # Obtener todos los usuarios.
         userL = getAllUserIDs()
         assert len(userL)>0
+        nodesD = sParser.nodesD # Node(clickDate, user_id, urls_id, profile,microNode)
 
         # Extraer sesiones para cada usuario, dado un tiempo limite entre pasos.
-        userSessionsD = dict()
-        for user_id in userL:
-            user_steps = self.extractFilteredSessionsOf(user_id,nodes)
-            if len(user_steps)>0:
-                userSessionsD[user_id] = user_steps
-        assert len(userSessionsD) > 0
-        #(k, x[0], x[1], x[2], x[3])
         sessions = list()
-        for k in userSessionsD.keys():
-            for x in userSessionsD[k]:
-                s = (k,x[0],x[1],x[2],x[3])
-                node = self.getFirstNode(s)
-                sessions.append(Session(node,initTime=s[3],endTime=s[4]))
+        for user_id in userL:
+            user_sessions = self.extractFilteredSessionsOf(nodesD[user_id]) # Array of Session() objects.
+            if len(user_sessions)>0:
+                for s in user_sessions:
+                    sessions.append(s)
+        assert len(sessions) > 0
         return sessions
 
     def getFirstNode(self,s):
@@ -89,20 +81,20 @@ class FilteredSessionizer(Sessionizer):
         tp = firstStep.split(',')
         macro_id = tp[0]
         micro_id = tp[1]
-        firstNode = Node(id_user=s[0], profile=s[1], id_url=macro_id,microNode=micro_id)
+        firstNode = Node(id_user=s[0], profile=s[1], id_url=macro_id, microNode=micro_id)
         currentNode = firstNode
         for step in steps[1:]:
             tp = step.split(',')
             macro_id = tp[0]
             micro_id = tp[1]
-            newNode = Node(id_user=s[0], profile=s[1], id_url=macro_id,microNode=micro_id)
+            newNode = Node(id_user=s[0], profile=s[1], id_url=macro_id, microNode=micro_id)
             currentNode.addNext(newNode)
             currentNode = currentNode.next
         return firstNode
 
     def __toSession(self, profile, sessionData, initTime, endTime):
-        return profile,' '.join([','.join([str(i) for i in x]) for x in sessionData]), datetime.fromtimestamp(initTime).isoformat(' '), datetime.fromtimestamp(endTime).isoformat(' ')
-
+        #return profile,' '.join([','.join([str(i) for i in x]) for x in sessionData]), datetime.fromtimestamp(initTime).isoformat(' '), datetime.fromtimestamp(endTime).isoformat(' ')
+        return Session(sessionData,initTime=datetime.fromtimestamp(initTime).isoformat(' '), endTime=datetime.fromtimestamp(endTime).isoformat(' '))
 
 class StepFilter:
 
@@ -111,7 +103,7 @@ class StepFilter:
 class LastMacroIDStepFilter(StepFilter):
 
     def accepts(self,prevStep, step):
-        if prevStep[1] == step[1]:
+        if prevStep.urls_id == step.urls_id:# and prevStep.profile == step.profile:
             return False
         else:
             return True

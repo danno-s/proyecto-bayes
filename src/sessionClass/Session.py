70729500574852439954e7c238052eab413e11b1
sebastian <sebago.nc@gmail.com>
2016-01-28 17:46:39 -0300

    modificado Sessions para trabajar con listas de tuplas de IDs.

--- src/sessionClass/Session.py
+++ src/sessionClass/Session.py
@@ -9,7 +9,7 @@ class Session:
 
         if profile:
             self.profile = profile
-        self.sequence = sequence   #string with format "1,2 2,44 3,1" (con microNodos) o "1 2 3 4" (sin microNodos)
+        self.sequence = sequence   # lista de tuplas (urls_id, micro_id), o (urls_id, None)
 
     def __getSequenceFromNode(self,node):
         sequence = list()
@@ -18,32 +18,29 @@ class Session:
                 if node.microNode:
                     sequence.append((node.urls_id, node.microNode))
                 else:
-                    sequence.append((node.urls_id,""))
+                    sequence.append((node.urls_id,None))
             node = node.next
         return sequence
 
-    def getFirstNode(self,s):
+    def getFirstNode(self,steps):
         """
 
         Parameters
         ----------
-        s : a session str with format "1,2 2,44 3,1" (con microNodos) o "1 2 3 4" (sin microNodos)
+        steps : lista de tuplas (urls_id, micro_id), o (urls_id, None)
 
         Returns
         -------
         a Node object with the nested list of the session.
         """
-        steps = s.split(' ')
         firstStep = steps[0]
-        tp = firstStep.split(',')
-        macro_id = tp[0]
-        micro_id = tp[1]
+        macro_id = firstStep[0]
+        micro_id = firstStep[1]
         firstNode = Node(id_user=self.user_id, profile=self.profile, id_url=macro_id,microNode=micro_id)
         currentNode = firstNode
         for step in steps[1:]:
-            tp = step.split(',')
-            macro_id = tp[0]
-            micro_id = tp[1]
+            macro_id = step[0]
+            micro_id = step[1]
             newNode = Node(id_user=self.user_id, profile=self.profile, id_url=macro_id,microNode=micro_id)
             currentNode.addNext(newNode)
             currentNode = currentNode.next
@@ -52,4 +49,14 @@ class Session:
 
 
     def __str__(self):
-        return str(self.profile)+":\t"+str(self.sequence) +" :\t " + str(self.initTime) +" >> "+ str(self.endTime)
+        return str(self.profile)+":\t"+self.__sequenceToStr(self.sequence) +" ;\t " + str(self.initTime) +" >> "+ str(self.endTime)
+
+    def __sequenceToStr(self,sequence):
+        tps = list()
+        for x in sequence:
+            if x[1]:
+                tps.append("("+ str(x[0])+","+str(x[1])+")")
+            else:
+                tps.append("("+ str(x[0])+")")
+        s = ' >> '.join(tps)
+        return s

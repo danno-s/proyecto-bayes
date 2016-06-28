2ab8108fc9ae1175865f4e6cf5b0f6b701522244
Pablo Reszczynski <pablore@me.com>
2016-06-28 01:29:01 -0400

    Falta encontrar la manera de subir asignar las sesiones a los usuarios

--- src/simulatedData/simulusers.py
+++ src/simulatedData/simulusers.py
@@ -14,9 +14,11 @@ import pprint
 from src.utils.loadConfig import Config
 from src.utils.sqlUtils import sqlWrapper
 
+DEBUG = False
+
 def cleanJSON(JSONFile):
     print("path: "+os.path.dirname(os.path.abspath(__file__)))
-    dirname = os.path.dirname(os.path.abspath(__file__)) + "/src/simulatedData/"
+    dirname = os.path.dirname(os.path.abspath(__file__)) + "/simulatedData/"
     commentREGEX = re.compile('/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/', re.DOTALL | re.MULTILINE)
     with open(dirname + JSONFile) as J:
         content = ''.join(J.readlines())
@@ -29,7 +31,8 @@ def cleanJSON(JSONFile):
 
 conf = cleanJSON("simulConfig.json")
 
-def simulusers(n1=70, n2=23, n3=7, n=200):  # TODO: estos parametros deberian ser configurados tambien, sacar desde N_clusters
+def simulusers(params, cluster, n):
+# TODO: estos parametros deberian ser configurados tambien, sacar desde N_clusters
     """
     Genera usuarios simulados con un id, username y perfil
 
@@ -44,6 +47,7 @@ def simulusers(n1=70, n2=23, n3=7, n=200):  # TODO: estos parametros deberian se
         Lista de usuarios con id y perfil
 
     """
+
     sqlPD = sqlWrapper(db='PD')
 
     usernQuery = "SELECT username FROM users WHERE simulated = 0"
@@ -53,24 +57,33 @@ def simulusers(n1=70, n2=23, n3=7, n=200):  # TODO: estos parametros deberian se
     for i in range(n):
         user[i] = random.choice(usern)
 
-    user_id = random.sample(range(4000), n)  # ids generados al azar
-    profile = [0] * int(n1 * n / 100) + [1] * int(n2 * n / 100) + \
-        [2] * int(n3 * n / 100)  # perfiles de usuario
-    random.shuffle(profile)
+    user_id = random.sample(range(80000), n)  # ids generados al azar
+
+    #random.shuffle(profile)
                    # TODO: en lugar de asignarles un perfil, usar dirichlet
                    # para asignar sesiones
 
     readParams = "user_id,username,profile"
     sqlWrite = "INSERT INTO users (user_id,username,profile) VALUES (%s, %s, %s)"
-    sqlPD.truncateSimulated("users", readParams, sqlWrite)
+    #sqlPD.truncateSimulated("users", readParams, sqlWrite)
     # Guardar usuarios
     sqlWrite = "INSERT INTO users (user_id,username,profile, simulated, label) VALUES (%s, %s, %s, %s ,%s)"
-    #print(user_id)
-    for i in range(len(user_id)):
-        # TODO: Insertar label de cluster (a priori).
-        sqlPD.write(sqlWrite, (user_id[i], user[i], profile[i], True, None))
 
-    return list(zip(user_id, profile))
+    #print(len(user_id), len(user))
+    for i in range(n):
+        n1 = params[i][0][0];
+        n2 = params[i][0][1];
+        n3 = params[i][0][2];
+
+        profile = [0] * int(n1 * n / 100) + [1] * int(n2 * n / 100) + \
+            [2] * int(n3 * n / 100)
+        diff = n - len(profile)
+        if diff > 0:
+            for j in range(diff):
+                profile.append(2)
+        sqlPD.write(sqlWrite, (user_id[i], user[i], profile[i], True, cluster))
+
+    return list(zip(user_id, profile, [cluster] * n))
 
 
 def __vect(n):
@@ -226,29 +239,113 @@ def cleanDB():
 def showSimulated():
     sqlCD = sqlWrapper(db='CD')
     sqlPD = sqlWrapper(db='PD')
-    getUsers = "SELECT * FROM users WHERE simulated = 1"
+    getUsers = "SELECT * FROM users WHERE simulated = 1 AND label = %s"
     getSessions = "SELECT * FROM sessions WHERE simulated = 1"
     getNodes = "SELECT * FROM nodes WHERE simulated = 1"
-    print("Users:")
-    print([i[0] for i in sqlPD.read(getUsers)])
+    getClusters = "SELECT DISTINCT label FROM users WHERE simulated = 1"
+
+    print("Clusters:")
+    clusters = [i[0] for i in sqlPD.read(getClusters)]
+    print(clusters)
+
+    for label in clusters:
+        if (label == None): pass
+        print("Label: " + str(label))
+        print("\tUsers:")
+        print([i[0] for i in sqlPD.read(getUsers % label)])
     print("Sessions:")
     print([i[0] for i in sqlCD.read(getSessions)])
     print("Nodes:")
     print([i[0] for i in sqlCD.read(getNodes)])
 
 def dirichlet(alpha=[70,24,6], size=200):
+    """
+    Genera multinomiales para un cluster a partir de una distribucion de dirichlet.
+
+    Parameters
+    __________
+    alpha: [Int]
+        Lista que representa el vector alpha de la distribucion
+        de Dirichlet.
+    size: Int
+        Cantidad de multinomiales por generar.
+
+    Returns
+    _______
+    np.Array
+        Arreglo de Numpy de multinomiales generadas
+    """
     dirich = np.random.dirichlet(alpha, size)
-    #print(dirich)
+    #print(dirich[0])
     multis = []
     for u in range(size):
         multi = np.random.multinomial(n=100, pvals=dirich[u], size=1)
         #print(multi)
-        multis.append(multi.tolist())
+        multis.append(multi)
+    assert(len(multis) == size)
     return multis
 
 
+def newGenerate():
+    n_Clusters = conf["N_clusters"]
+    n_usuarios = conf["N_usuarios"]
+    param_dirich = conf["Param_Dirich"]
+
+    try:
+        assert(len(param_dirich) == n_Clusters)
+        assert(len(param_dirich) == len(n_usuarios))
+    except(AssertionError):
+        print ("numero de clusters != n de parametros")
+        raise ValueError
+
+    sqlCD = sqlWrapper(db='CD')
+
+    multis = []
+
+    # Se generan los usuarios
+    users = []
+    for i in range(n_Clusters):
+        parametros = dirichlet(param_dirich[i], n_usuarios[i])
+        new_users = simulusers(parametros, i,  n_usuarios[i])
+        multis.append(parametros)
+        users += new_users
+
+    sqlWrite = "INSERT INTO nodes (user_id, clickDate, urls_id, profile,\
+    micro_id, simulated, label) VALUES " \
+               "(%s,%s,%s,%s,%s,%s,%s)"
+    # Se asignan las sesiones
+    sessions = getsession()
+    numSessions = len(sessions)
+
+    for i in range(numSessions):
+        for user in users: #(id, perfil, label)
+            userId = user[0]
+            date = random.randint(1450000000, 1462534931)  # Timestamp de inicio
+            profile = user[1]
+            label = user[2]
+
+            userIdx = users.index(user)
+            multi = multis[label][userIdx].tolist()
+            print(multi)
+            idx = multi[0].index(max(multi[0]))
+
+            #TODO asignar sesion al usuario. Como lo hago? Se deben usar los parametros de la multi?
+
+            session = sessions[idx]
+            for subSession in session:
+                if type(subSession) is str:
+                    url = [int(x) for x in subSession.split(",")]
+                    microId = url[1]
+                    url = url[0]
+                else:
+                    url = subSession
+                    microId = None
+                insert = [userId, date, url, profile, microId, True, label]
+                sqlCD.write(sqlWrite, insert)
+
 def generate():
     """
+    DEPRECATED
     Genera los datos simulados y los guarda en la tabla simulatednodes
 
     Parameters
@@ -258,20 +355,20 @@ def generate():
     ufrac = simulationConfig["userfrac"]
     sfrac = simulationConfig["sessionfrac"]
     numSessions = simulationConfig["sessions"]
+
     users = simulusers(ufrac["n1"], ufrac["n2"], ufrac[
                        "n3"], simulationConfig["users"])
     session = getsession()
-
-    dirich = dirichlet([sfrac["n1"], sfrac["n2"], sfrac["n3"]], len(users))
 
     sprob = __probses(len(session), sfrac["n1"], sfrac["n2"])
     prob = __probtable(
         list({len(x) for y in session for x in y if len(x) >= 3}))
 
-    pprint.pprint("dirich: " + str(dirich))
-    pprint.pprint("session: " + str(session))
-    pprint.pprint("sprob: " + str(sprob))
-    pprint.pprint("prob: " + str(prob))
+    if _DEBUG:
+        pprint.pprint("dirich: " + str(dirich))
+        pprint.pprint("session: " + str(session))
+        pprint.pprint("sprob: " + str(sprob))
+        pprint.pprint("prob: " + str(prob))
 
     sqlCD = sqlWrapper(db='CD')
     readParams = "user_id, clickDate, urls_id, profile, micro_id"
@@ -288,7 +385,7 @@ def generate():
             # segun el perfil
             p = sprob[u[1]]
             idx = np.random.multinomial(1, p, size=1).tolist()[0].index(
-                1)  # Se guarda su índice para poder etiquetar
+                    1)  # Se guarda su índice para poder etiquetar
             ses = session[idx]  # Se elige una sesion
             # idx = l.index(ses) + 1
             # TODO: desde aqui en adelante es lo mismo
@@ -310,7 +407,9 @@ def generate():
     print ("Ok, %s users simulated, %s sessions simulated, %s nodes created" % (len(users), numSessions, numSessions * len(users)))
 
 if __name__ == '__main__':
+    global _DEBUG
+    _DEBUG = True
     #dirichlet()
     cleanDB()
-    generate()
-    #showSimulated()
+    newGenerate()
+    showSimulated()
